3eb0ba4b7027661ad0eb1339d63e6a5dbebd9db7
Ekansh Sharma <eku130902@gmail.com>
2024-06-09 12:11:51 +0530

    secon

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -70,7 +70,7 @@ function Navbar() {
                         <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
                         {loading ? (
                           <p className="text-center">Loading...</p>
-                        ) : subLinks.length > 0 ? (
+                        ) : (Array.isArray(subLinks) && subLinks.length > 0) ? (
                           <>
                             {subLinks
                               ?.filter(
